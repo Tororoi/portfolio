1f6557b546da2d65a11100b9d174f512d9b51ac5
Tororoi <cantwell.tom@gmail.com>
2020-08-20 18:40:34 -0400

    added blogs to main page

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends React.Component {
             } />
            <Route path="/" exact render={() =>
              <ProjectContainer/>
+             <BlogContainer/>
             } />
           </main>
         </Router>
